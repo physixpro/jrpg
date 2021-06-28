a98c8a5d0f74020858ed89ff7daff4aee1c084d1
Keagan St. Rose <keaganstrose@gmail.com>
2021-06-28 12:47:38 -0700

    first commit again

---
+++ src/components/Sprite.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Sprite = ({image, data}) => {
+  const {y, x, h, w} = data;
+  return (
+    <div
+      style={{
+        display: "inline-block",
+        height: `${h}px`,
+        width: `${w}px`,
+        backgroundImage:
+          `url(${image})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: `-${x}px -${y}px`,
+      }}
+      >
+    </div>
+  );
+};
+export default Sprite;
